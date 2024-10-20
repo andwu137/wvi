10201c0bcd92b25a121fdea0d502cf7ee25747d8
neck <neck@localhost.localdomain>
2024-10-20 18:06:38 -0400

    feat(buffer_map): made a wrapper around hashmap for file buffer

--- src/wvi/buffer_map.rs
+++ src/wvi/buffer_map.rs
@@ -1,1 +1,36 @@
 use crate::wvi::file_buffer;
+use std::collections;
+
+struct BufferMap {
+    buffers: collections::HashMap<String, file_buffer::FileBuffer>,
+}
+
+impl BufferMap {
+    fn add_buffer(
+        &mut self,
+        name: &String,
+        buf: file_buffer::FileBuffer,
+    ) -> Option<file_buffer::FileBuffer> {
+        self.add_buffer_owned(name.clone(), buf)
+    }
+
+    fn add_buffer_owned(
+        &mut self,
+        name: String,
+        buf: file_buffer::FileBuffer,
+    ) -> Option<file_buffer::FileBuffer> {
+        self.buffers.insert(name, buf)
+    }
+
+    fn rm_buffer(&mut self, name: &String) -> Option<file_buffer::FileBuffer> {
+        self.buffers.remove(name)
+    }
+
+    fn get_buffer(&self, name: &String) -> Option<&file_buffer::FileBuffer> {
+        self.buffers.get(name)
+    }
+
+    fn get_mut_buffer(&mut self, name: &String) -> Option<&mut file_buffer::FileBuffer> {
+        self.buffers.get_mut(name)
+    }
+}
