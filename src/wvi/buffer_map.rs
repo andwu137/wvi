cf013e684804de48a29f610fa4bb35c58d64174e
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-10-20 19:17:43 -0400

    style(buffer_map): dont qualify import types(struct/enum) unless ambiguous

--- src/wvi/buffer_map.rs
+++ src/wvi/buffer_map.rs
@@ -1,36 +1,28 @@
-use crate::wvi::file_buffer;
+use super::file_buffer::FileBuffer;
 use std::collections;
 
 struct BufferMap {
-    buffers: collections::HashMap<String, file_buffer::FileBuffer>,
+    buffers: collections::HashMap<String, FileBuffer>,
 }
 
 impl BufferMap {
-    fn add_buffer(
-        &mut self,
-        name: &String,
-        buf: file_buffer::FileBuffer,
-    ) -> Option<file_buffer::FileBuffer> {
+    fn add_buffer(&mut self, name: &String, buf: FileBuffer) -> Option<FileBuffer> {
         self.add_buffer_owned(name.clone(), buf)
     }
 
-    fn add_buffer_owned(
-        &mut self,
-        name: String,
-        buf: file_buffer::FileBuffer,
-    ) -> Option<file_buffer::FileBuffer> {
+    fn add_buffer_owned(&mut self, name: String, buf: FileBuffer) -> Option<FileBuffer> {
         self.buffers.insert(name, buf)
     }
 
-    fn rm_buffer(&mut self, name: &String) -> Option<file_buffer::FileBuffer> {
+    fn rm_buffer(&mut self, name: &String) -> Option<FileBuffer> {
         self.buffers.remove(name)
     }
 
-    fn get_buffer(&self, name: &String) -> Option<&file_buffer::FileBuffer> {
+    fn get_buffer(&self, name: &String) -> Option<&FileBuffer> {
         self.buffers.get(name)
     }
 
-    fn get_mut_buffer(&mut self, name: &String) -> Option<&mut file_buffer::FileBuffer> {
+    fn get_mut_buffer(&mut self, name: &String) -> Option<&mut FileBuffer> {
         self.buffers.get_mut(name)
     }
 }
