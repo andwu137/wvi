6cb04955eec486a9dbb26642affd8d617aa1bd62
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-10-20 20:20:10 -0400

    feat(modes/normal): assert cursor is less than its bounding buffer

--- src/wvi/modes/normal.rs
+++ src/wvi/modes/normal.rs
@@ -15,19 +15,22 @@ impl Normal {
     // -> not go beyond lowest line in buf
     fn move_cursor(&mut self, buf: &FileBuffer, dir: Direction) {
         let new_pos = match dir {
-            // TODO: ASSERT
             Direction::Up => self.cursor.add(&V2::new(0, 1)),
             Direction::Down => self.cursor.sat_sub(&V2::new(0, 1)),
             Direction::Left => self.cursor.sat_sub(&V2::new(1, 0)),
             Direction::Right => self.cursor.add(&V2::new(1, 0)),
         };
+
+        assert!(self.cursor.y < buf.len());
     }
 
     fn display_cursor(&self, buf: &FileBuffer) -> V2 {
         assert!(self.cursor.y < buf.len());
-        V2::clamp(
-            &self.cursor,
-            &V2::new(buf.line_len(self.cursor.y).unwrap(), self.cursor.y),
-        )
+        let cur = self.cursor.clamp(&V2::new(
+            buf.line_len(self.cursor.y).unwrap(),
+            self.cursor.y,
+        ));
+        assert!(cur.y < buf.len());
+        cur
     }
 }
