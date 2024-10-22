d21091b9503a0f47458ab3a12545a565f6a4247b
Andrew Nguyen <nguyenandrew137@protonmail.com>
2024-10-22 19:05:25 -0400

    feat(modes.normal): finished move_cursor func

--- src/wvi/modes/normal.rs
+++ src/wvi/modes/normal.rs
@@ -14,11 +14,14 @@ impl Normal {
     // -> not go beyond rightmost character on that line
     // -> not go beyond lowest line in buf
     fn move_cursor(&mut self, buf: &FileBuffer, dir: Direction) {
-        let new_pos = match dir {
-            Direction::Up => self.cursor.add(&V2::new(0, 1)),
-            Direction::Down => self.cursor.sat_sub(&V2::new(0, 1)),
-            Direction::Left => self.cursor.sat_sub(&V2::new(1, 0)),
-            Direction::Right => self.cursor.add(&V2::new(1, 0)),
+        self.cursor = match dir {
+            Direction::Up => self.cursor.sat_sub(&V2::new(0, 1)),
+            Direction::Down => self.cursor.add(&V2::new(0, 1)).clamp_y(buf.len()),
+            Direction::Left => self.display_cursor(buf).sat_sub(&V2::new(1, 0)),
+            Direction::Right => self
+                .display_cursor(buf)
+                .add(&V2::new(1, 0))
+                .clamp_x(buf.line_len(self.cursor.y).unwrap()),
         };
 
         assert!(self.cursor.y < buf.len());
